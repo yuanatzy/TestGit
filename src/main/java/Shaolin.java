9cb2164668255101ee6b3ea624bf8ebc581eff90
eclipse_git <eclipse_git@qq.com>
2022-07-05 22:30:18 +0800

    test push

--- src/main/java/Shaolin.java
+++ src/main/java/Shaolin.java
@@ -4,6 +4,7 @@ public class Shaolin {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("我少林表示不服，毕竟张三丰武功源出少林");
+		System.out.println("我少林表示不服，毕竟张三丰武功源出少林");
 	}
 
 }
