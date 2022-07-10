2dfa3915899bc4d6b5373de37c048e5c23730f34
eclipse_git <eclipse_git@qq.com>
2022-07-10 17:01:10 +0800

    hot fix...

--- src/main/java/Shaolin.java
+++ src/main/java/Shaolin.java
@@ -5,6 +5,7 @@ public class Shaolin {
 		// TODO Auto-generated method stub
 		System.out.println("我少林表示不服，毕竟张三丰武功源出少林");
 		System.out.println("我少林表示不服，毕竟张三丰武功源出少林");
+		System.out.println("人海啊，茫茫啊。。。");
 	}
 
 }
