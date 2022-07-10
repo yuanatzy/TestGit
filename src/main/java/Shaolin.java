693de916844ccdea557fa4e694b513e601cdc09f
ycl_glb <ycl_glb@qq.com>
2022-07-10 13:39:27 +0800

    shaolin test conlict

--- src/main/java/Shaolin.java
+++ src/main/java/Shaolin.java
@@ -4,7 +4,7 @@ public class Shaolin {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("我少林表示不服，毕竟张三丰武功源出少林");
-		System.out.println("少林72绝技，最多能练多少门？");
+		System.out.println("少林72绝技，最多能练多少门？test conflict");
 	}
 
 }
